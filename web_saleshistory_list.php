98e067d5f52d96c0b92a5519a11ad76dee6fdd7d
sakuraitsubute <65934184+sakuraitsubute@users.noreply.github.com>
2020-07-20 10:03:09 +0900

    Update web_saleshistory_list.php

--- web_saleshistory_list.php
+++ web_saleshistory_list.php
@@ -46,157 +46,242 @@ $fm->setProperty('hostspec', 'http://192.168.0.73');
 $fm->setProperty('username', 'web');
 $fm->setProperty('password', 'tEL6728061');
 
-//直近一週間で棚卸しをした記録があるかどうか調べる
+//当月の各営業コードごとに売上情報を検索して変数に格納
 
-$find01to31 = $fm->newFindcommand('売上履歴');
-$find01to31->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
-$find01to31->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
-$result = $find01to31->execute();
+$find01to31_a = $fm->newFindcommand('売上履歴');
+$find01to31_a->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_a->addFindCriterion('受注番号_CHAR', 'A0');
+//$find01to31_a->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_a = $find01to31_a->execute();
+if(!FileMaker::isError($result_a)){
+  $records_a = $result_a->getRecords();
+}
 
-if(FileMaker::isError($result)){
-  echo 'FileMaker Error:'.$result->getCode().'<br>';
-  echo $result->getMessage();
-  echo '<h1>当月の売上データがありません</h1>';
-  echo' <p><button type="button" id="close">閉じる</button></p>';
-}else{
-  $records = $result->getRecords();
+$find01to31_b = $fm->newFindcommand('売上履歴');
+$find01to31_b->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_b->addFindCriterion('受注番号_CHAR', 'B0');
+//$find01to31_b->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_b = $find01to31_b->execute();
+if(!FileMaker::isError($result_b)){
+  $records_b = $result_b->getRecords();
+}
 
-  $A0 = array();
-  $B0 = array();
-  $C0 = array();
-  $D0 = array();
-  $E0 = array();
-  $F0 = array();
-  $G0 = array();
-  $H0 = array();
-  $I0 = array();
-  $J0 = array();
-  $K0 = array();
-  $L0 = array();
-  $M0 = array();
-  $N0 = array();
-  $O0 = array();
-  $P0 = array();
-  $Q0 = array();
-  $R0 = array();
-  $S0 = array();
-  $T0 = array();
-  $U0 = array();
-  $V0 = array();
-  $W0 = array();
-  $X0 = array();
-  $Y0 = array();
-  $Z0 = array();
-  
-  foreach($records as $record){
-    $order = $record->getFIeld('受注番号_CHAR');
-    switch(substr($order,0,2)){
-      case 'A0':
-        $A0[] = $record;
-      break;
+$find01to31_c = $fm->newFindcommand('売上履歴');
+$find01to31_c->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_c->addFindCriterion('受注番号_CHAR', 'C0');
+//$find01to31_c->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_c = $find01to31_c->execute();
+if(!FileMaker::isError($result_c)){
+  $records_c = $result_c->getRecords();
+}
 
-      case 'B0':
-        $B0[] = $record;
-      break;
+$find01to31_d = $fm->newFindcommand('売上履歴');
+$find01to31_d->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_d->addFindCriterion('受注番号_CHAR', 'D0');
+//$find01to31_d->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_d = $find01to31_d->execute();
+if(!FileMaker::isError($result_d)){
+  $records_d = $result_d->getRecords();
+}
 
-      case 'C0':
-        $C0[] = $record;
-      break;
+$find01to31_e = $fm->newFindcommand('売上履歴');
+$find01to31_e->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_e->addFindCriterion('受注番号_CHAR', 'E0');
+//$find01to31_e->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_e = $find01to31_e->execute();
+if(!FileMaker::isError($result_e)){
+  $records_e = $result_e->getRecords();
+}
 
-      case 'D0':
-        $D0[] = $record;
-      break;
+$find01to31_f = $fm->newFindcommand('売上履歴');
+$find01to31_f->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_f->addFindCriterion('受注番号_CHAR', 'F0');
+//$find01to31_f->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_f = $find01to31_f->execute();
+if(!FileMaker::isError($result_f)){
+  $records_f = $result_f->getRecords();
+}
 
-      case 'E0':
-        $E0[] = $record;
-      break;
+$find01to31_g = $fm->newFindcommand('売上履歴');
+$find01to31_g->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_g->addFindCriterion('受注番号_CHAR', 'G0');
+//$find01to31_g->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_g = $find01to31_g->execute();
+if(!FileMaker::isError($result_g)){
+  $records_g = $result_g->getRecords();
+}
 
-      case 'F0':
-        $F0[] = $record;
-      break;
+$find01to31_h = $fm->newFindcommand('売上履歴');
+$find01to31_h->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_h->addFindCriterion('受注番号_CHAR', 'H0');
+//$find01to31_h->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_h = $find01to31_h->execute();
+if(!FileMaker::isError($result_h)){
+  $records_h = $result_h->getRecords();
+}
 
-      case 'G0':
-        $G0[] = $record;
-      break;
+$find01to31_i = $fm->newFindcommand('売上履歴');
+$find01to31_i->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_i->addFindCriterion('受注番号_CHAR', 'I0');
+//$find01to31_i->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_i = $find01to31_i->execute();
+if(!FileMaker::isError($result_i)){
+  $records_i = $result_i->getRecords();
+}
 
-      case 'H0':
-        $H0[] = $record;
-      break;
+$find01to31_j = $fm->newFindcommand('売上履歴');
+$find01to31_j->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_j->addFindCriterion('受注番号_CHAR', 'J0');
+//$find01to31_j->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_j = $find01to31_j->execute();
+if(!FileMaker::isError($result_j)){
+  $records_j = $result_j->getRecords();
+}
 
-      case 'I0':
-        $I0[] = $record;
-      break;
+$find01to31_k = $fm->newFindcommand('売上履歴');
+$find01to31_k->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_k->addFindCriterion('受注番号_CHAR', 'K0');
+//$find01to31_k->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_k = $find01to31_k->execute();
+if(!FileMaker::isError($result_k)){
+  $records_k = $result_k->getRecords();
+}
 
-      case 'J0':
-        $J0[] = $record;
-      break;
+$find01to31_l = $fm->newFindcommand('売上履歴');
+$find01to31_l->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_l->addFindCriterion('受注番号_CHAR', 'L0');
+//$find01to31_l->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_l = $find01to31_l->execute();
+if(!FileMaker::isError($result_l)){
+  $records_l = $result_l->getRecords();
+}
 
-      case 'K0':
-        $K0[] = $record;
-      break;
+$find01to31_m = $fm->newFindcommand('売上履歴');
+$find01to31_m->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_m->addFindCriterion('受注番号_CHAR', 'M0');
+$find01to31_m->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_m = $find01to31_m->execute();
+if(!FileMaker::isError($result_m)){
+  $records_m = $result_m->getRecords();
+}
 
-      case 'L0':
-        $L0[] = $record;
-      break;
+$find01to31_n = $fm->newFindcommand('売上履歴');
+$find01to31_n->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_n->addFindCriterion('受注番号_CHAR', 'N0');
+//$find01to31_n->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_n = $find01to31_n->execute();
+if(!FileMaker::isError($result_n)){
+  $records_n = $result_n->getRecords();
+}
 
-      case 'M0':
-        $M0[] = $record;
-      break;
+$find01to31_o = $fm->newFindcommand('売上履歴');
+$find01to31_o->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_o->addFindCriterion('受注番号_CHAR', 'O0');
+//$find01to31_o->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_o = $find01to31_o->execute();
+if(!FileMaker::isError($result_o)){
+  $records_o = $result_o->getRecords();
+}
 
-      case 'N0':
-        $N0[] = $record;
-      break;
+$find01to31_p = $fm->newFindcommand('売上履歴');
+$find01to31_p->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_p->addFindCriterion('受注番号_CHAR', 'P');
+//$find01to31_p->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_p = $find01to31_p->execute();
+if(!FileMaker::isError($result_p)){
+  $records_p = $result_p->getRecords();
+}
 
-      case 'O0':
-        $O0[] = $record;
-      break;
+$find01to31_q = $fm->newFindcommand('売上履歴');
+$find01to31_q->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_q->addFindCriterion('受注番号_CHAR', 'Q0');
+//$find01to31_q->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_q = $find01to31_q->execute();
+if(!FileMaker::isError($result_q)){
+  $records_q = $result_q->getRecords();
+}
 
-      case 'P0':
-        $P0[] = $record;
-      break;
+$find01to31_r = $fm->newFindcommand('売上履歴');
+$find01to31_r->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_r->addFindCriterion('受注番号_CHAR', 'R0');
+//$find01to31_r->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_r = $find01to31_r->execute();
+if(!FileMaker::isError($result_r)){
+  $records_r = $result_r->getRecords();
+}
 
-      case 'Q0':
-        $Q0[] = $record;
-      break;
+$find01to31_s = $fm->newFindcommand('売上履歴');
+$find01to31_s->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_s->addFindCriterion('受注番号_CHAR', 'S0');
+//$find01to31_s->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_s = $find01to31_s->execute();
+if(!FileMaker::isError($result_s)){
+  $records_s = $result_s->getRecords();
+}
 
-      case 'R0':
-        $R0[] = $record;
-      break;
+$find01to31_t = $fm->newFindcommand('売上履歴');
+$find01to31_t->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_t->addFindCriterion('受注番号_CHAR', 'T0');
+//$find01to31_t->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_t = $find01to31_t->execute();
+if(!FileMaker::isError($result_t)){
+  $records_t = $result_t->getRecords();
+}
 
-      case 'S0':
-        $S0[] = $record;
-      break;
+$find01to31_u = $fm->newFindcommand('売上履歴');
+$find01to31_u->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_u->addFindCriterion('受注番号_CHAR', 'U0');
+//$find01to31_u->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_u = $find01to31_u->execute();
+if(!FileMaker::isError($result_u)){
+  $records_u = $result_u->getRecords();
+}
 
-      case 'T0':
-        $T0[] = $record;
-      break;
+$find01to31_v = $fm->newFindcommand('売上履歴');
+$find01to31_v->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_v->addFindCriterion('受注番号_CHAR', 'V0');
+//$find01to31_v->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_v = $find01to31_v->execute();
+if(!FileMaker::isError($result_v)){
+  $records_v = $result_v->getRecords();
+}
 
-      case 'U0':
-        $U0[]= $record;
-      break;
+$find01to31_w = $fm->newFindcommand('売上履歴');
+$find01to31_w->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_w->addFindCriterion('受注番号_CHAR', 'W0');
+//$find01to31_w->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_w = $find01to31_w->execute();
+if(!FileMaker::isError($result_w)){
+  $records_w = $result_w->getRecords();
+}
 
-      case 'V0':
-        $V0[] = $record;
-      break;
+$find01to31_x = $fm->newFindcommand('売上履歴');
+$find01to31_x->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_x->addFindCriterion('受注番号_CHAR', 'X0');
+//$find01to31_x->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_x = $find01to31_x->execute();
+if(!FileMaker::isError($result_x)){
+  $records_x = $result_x->getRecords();
+}
 
-      case 'W0':
-        $W0[] = $record;
-      break;
+$find01to31_y = $fm->newFindcommand('売上履歴');
+$find01to31_y->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+//$find01to31_y->addFindCriterion('受注番号_CHAR', 'Y0');
+$find01to31_y->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_y = $find01to31_y->execute();
+if(!FileMaker::isError($result_y)){
+  $records_y = $result_y->getRecords();
+}
 
-      case 'X0':
-        $X0[] = $record;
-      break;
-
-      case 'Y0':
-        $Y0[] = $record;
-      break;
-
-      case 'Z0':
-        $Z0[] = $record;
-      break;
-    }
-  }
-  
+$find01to31_z = $fm->newFindcommand('売上履歴');
+$find01to31_z->addFindCriterion('年月日_UNSIGN_NUMERIC', date("Ym01", time()).'...'.date("Ymt", time()));
+$find01to31_z->addFindCriterion('受注番号_CHAR', 'Z0');
+//$find01to31_z->addSortRule('受注番号_CHAR', 1, FILEMAKER_SORT_ASCEND);
+$result_z = $find01to31_z->execute();
+if(!FileMaker::isError($result_z)){
+  $records_z = $result_z->getRecords();
+}
+var_dump($records_z)
 
 ?>
 
@@ -213,8 +298,8 @@ if(FileMaker::isError($result)){
 </thead>
 <tbody>
 <td>Z0</td>
-<td><?php foreach($Z0 as $data){$price_Z = $data->getField('金額_DICIMAL'); $price_total_Z +=$price_Z; var_dump($price_Z);} echo $price_total_Z; unset($data) ?></td>
-<td><?php foreach($Z0 as $data){$cost_Z = $data->getField('原価_DICIMAL'); $cost_total_Z += $cost_Z;} echo $cost_total_Z; unset($data) ?></td>
+<td><?php foreach($records_z as $data){$price_Z = $data->getField('金額_DICIMAL'); $price_total_Z +=$price_Z; var_dump($price_Z);} echo $price_total_Z; unset($data) ?></td>
+<td><?php foreach($records_z as $data){$cost_Z = $data->getField('原価_DICIMAL'); $cost_total_Z += $cost_Z;} echo $cost_total_Z; unset($data) ?></td>
 <td><?php $price_total_Z - $cost_total_Z ?></td>
 <td><?php ($cost_total_Z / $price_total_Z) * 100 ?></td>
 </tbody>
@@ -296,7 +381,7 @@ if(FileMaker::isError($result)){
 </p>
 <?php
   }
-}
+
 
 ?>
 
